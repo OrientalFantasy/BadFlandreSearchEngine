8f6ccb28f7893e7a56ba1fe1734d80612b600f89
whiterasbk <whiterasbk@qq.com>
2023-12-21 00:56:34 +0800

    add result card & localize libs

--- src/main/resources/webroot/js/script.js
+++ src/main/resources/webroot/js/script.js
@@ -1,3 +1,53 @@
 // 动态年份
 var yearSpan = document.getElementById("year");
 yearSpan.innerText = new Date().getFullYear();
+
+const { createApp, ref } = Vue;
+
+createApp({
+    setup() {
+        const message = ref('Hello vue!');
+        const data = [
+            {
+                title: "Brunch this weekend?",
+                url: "https://baidu.com",
+                content: "All Connors - I'll be in your neighborhood doing errands this weekend. Do you want ..."
+            },
+            {
+                title: "casca Brunch this weekend?",
+                url: "https://qq.com",
+                content: "casasca neighborhood doing errands this weekend. Do you want "
+            },
+            {
+                title: "casca Brunch this weekend?",
+                url: "https://qq.com",
+                content: "casasca neighborhood doing errands this weekend. Do you want "
+            },
+            {
+                title: "casca Brunch this weekend?",
+                url: "https://qq.com",
+                content: "casasca neighborhood doing errands this weekend. Do you want "
+            },
+            {
+                title: "casca Brunch this weekend?",
+                url: "https://qq.com",
+                content: "casasca neighborhood doing errands this weekend. Do you want "
+            },
+            {
+                title: "casca Brunch this weekend?",
+                url: "https://qq.com",
+                content: "casasca neighborhood doing errands this weekend. Do you want "
+            }
+        ]
+        return {
+            message,
+            data
+        }
+    }
+}).mount('#baka-app');
+
+
+function jumpTo(url) {
+    window.location.href = url
+}
+
