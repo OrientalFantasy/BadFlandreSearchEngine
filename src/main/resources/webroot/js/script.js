42a45c75878bc0ec29d94c099248636d4d3836a6
whiterasbk <whiterasbk@qq.com>
2023-12-21 23:45:09 +0800

    finish almost frontend work

--- src/main/resources/webroot/js/script.js
+++ src/main/resources/webroot/js/script.js
@@ -2,52 +2,118 @@
 var yearSpan = document.getElementById("year");
 yearSpan.innerText = new Date().getFullYear();
 
-const { createApp, ref } = Vue;
+const { createApp, ref, reactive, computed } = Vue;
 
 createApp({
     setup() {
-        const message = ref('Hello vue!');
-        const data = [
-            {
-                title: "Brunch this weekend?",
-                url: "https://baidu.com",
-                content: "All Connors - I'll be in your neighborhood doing errands this weekend. Do you want ..."
-            },
-            {
-                title: "casca Brunch this weekend?",
-                url: "https://qq.com",
-                content: "casasca neighborhood doing errands this weekend. Do you want "
-            },
-            {
-                title: "casca Brunch this weekend?",
-                url: "https://qq.com",
-                content: "casasca neighborhood doing errands this weekend. Do you want "
-            },
-            {
-                title: "casca Brunch this weekend?",
-                url: "https://qq.com",
-                content: "casasca neighborhood doing errands this weekend. Do you want "
-            },
-            {
-                title: "casca Brunch this weekend?",
-                url: "https://qq.com",
-                content: "casasca neighborhood doing errands this weekend. Do you want "
-            },
-            {
-                title: "casca Brunch this weekend?",
-                url: "https://qq.com",
-                content: "casasca neighborhood doing errands this weekend. Do you want "
-            }
-        ]
+
+        let keyword = ref('')
+        let totalItem = ref(0),
+            groupedData = ref([]),
+            // errorMsg = ref(null),
+            totalPage = ref(0)
+
+        let currentIndex = ref(0)
+        let currentList = ref([])
+        let isWaitingForData = ref(false)
+
         return {
-            message,
-            data
+            currentList,
+            totalItem,
+            totalPage,
+            isWaitingForData,
+            keyword,
+            currentPage: computed(() => currentIndex.value + 1),
+            previousPage() {
+                if (this.hasPreviousPage()) {
+                    currentList.value = groupedData.value[--currentIndex.value]
+                }
+            },
+            nextPage() {
+                if (this.hasNextPage()) {
+                    currentList.value = groupedData.value[++currentIndex.value]
+                }
+            },
+            hasNextPage() {
+                return currentIndex.value < totalPage.value - 1
+            },
+            hasPreviousPage() {
+                return currentIndex.value > 0
+            },
+            async onQueryClick () {
+                if (keyword.value == null || keyword.value.trim() === '') {
+                    mdui.snackbar({
+                        message: '请输入关键词!',
+                        position: 'top'
+                    });
+                    keyword.value = ''
+                } else {
+                    isWaitingForData.value = true
+                    try {
+                        let result = await query(keyword.value)
+                        if (result.totalItem === 0) {
+                            mdui.dialog({
+                                title: '小恶魔的温馨提示',
+                                content: '没有在图书馆找到有关条目哦~',
+                                buttons: [
+                                    {
+                                        text: '我知道了'
+                                    }
+                                ]
+                            })
+                            keyword.value = ''
+                        } else {
+                            totalItem.value = result.totalItem
+                            groupedData.value = result.groupedData
+                            // errorMsg.value = result.errorMsg
+                            totalPage.value = result.totalPage
+                            currentList.value = groupedData.value[currentIndex.value]
+                        }
+                    } catch (e) {
+                        mdui.dialog({
+                            title: '检索错误',
+                            content: e,
+                            buttons: [
+                                {
+                                    text: '确认'
+                                }
+                            ]
+                        });
+                    } finally {
+                        isWaitingForData.value = false
+                    }
+                }
+            }
         }
     }
 }).mount('#baka-app');
 
+async function query(keyword) {
+    return new Promise((resolve, reject) => {
+        fetch('a?submit=' + encodeURIComponent(keyword)).then((result) => {
+            let resJson = result.json()
+            if (resJson.errorMsg !== undefined) {
+                const data = resJson.data
+                const totalPage = Math.ceil(data.length / 15)
+                const groupedData = []
+                let maxItemPerPage = 15
+                for (let i = 0; i < totalPage; i++) {
+                    let group = []
+                    for (let j = i * maxItemPerPage; j < (i + 1) * maxItemPerPage; j++) {
+                        if (j < data.length) {
+                            group.push(data[j])
+                        } else break
+                    }
+                    groupedData.push(group)
+                }
 
-function jumpTo(url) {
-    window.location.href = url
+                resolve({
+                    groupedData,
+                    totalPage,
+                    totalItem: data.length,
+                })
+            } else reject(resJson.errorMsg)
+        }).catch((e) => reject("获取数据的时候发生错误 " + e))
+    })
 }
 
